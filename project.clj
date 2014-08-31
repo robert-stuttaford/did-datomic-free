19f913373da5920ad99477d676b2f8bdd2430dd5
Robert Stuttaford <robert@cognician.com>
2014-08-31 14:26:39 +0200

    Ignores. No snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject did-datomic-free/lein-template "0.1.0-SNAPSHOT"
+(defproject did-datomic-free/lein-template "0.1.0"
   :description "A basic template for working with Datomic Free. Includes logging configuration."
   :url "https://github.com/robert-stuttaford/did-datomic-free"
   :license {:name "Eclipse Public License"
