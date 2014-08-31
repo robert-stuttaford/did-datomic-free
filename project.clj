c2c2f78bc4af42438471366e79196e57e456709f
Robert Stuttaford <robert@cognician.com>
2014-08-31 15:23:36 +0200

    Remove API usage.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject did-datomic-free/lein-template "0.1.0"
+(defproject did-datomic-free/lein-template "0.1.1"
   :description "A basic template for working with Datomic Free. Includes logging configuration."
   :url "https://github.com/robert-stuttaford/did-datomic-free"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/robert-stuttaford/did-datomic-free"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
--- src/leiningen/new/did_datomic_free/core.clj
+++ src/leiningen/new/did_datomic_free/core.clj
@@ -1,10 +1,2 @@
 (ns {{ns-name}}.core
   (:require [datomic.api :as d]))
-
-(def uri "datomic:free://localhost:4334/{{ns-name}}")
-
-(d/create-database uri)
-
-(def conn (d/connect uri))
-
-(def db (d/db conn))
